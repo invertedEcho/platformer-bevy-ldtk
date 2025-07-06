ac737cc42288dd3097a7dd5235831bda631e7559
Jakob Stechow <jakob.stechow@pm.me>
2025-07-06 22:29:54 +0200

    feat: grid coordinates based player movement

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
-use bevy_ecs_ldtk::prelude::*;
+use bevy_ecs_ldtk::{prelude::*, utils::grid_coords_to_translation};
+
+const GRID_SIZE: i32 = 16;
 
 #[derive(Default, Component)]
 pub struct Player;
@@ -8,6 +10,8 @@ pub struct Player;
 #[from_entity_instance]
 struct PlayerBundle {
     player: Player,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
 
 #[derive(Component)]
@@ -24,7 +28,15 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.register_ldtk_entity::<PlayerBundle>("Player")
-            .add_systems(Update, (process_player, animate_sprite));
+            .add_systems(
+                Update,
+                (
+                    process_player,
+                    animate_sprite,
+                    move_player_from_input,
+                    translate_grid_coords_entities,
+                ),
+            );
     }
 }
 
@@ -75,5 +87,36 @@ fn animate_sprite(
                 };
             }
         }
+    }
+}
+
+fn move_player_from_input(
+    mut players: Query<&mut GridCoords, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let movement_direction = if input.just_pressed(KeyCode::KeyW) {
+        GridCoords::new(0, 1)
+    } else if input.just_pressed(KeyCode::KeyA) {
+        GridCoords::new(-1, 0)
+    } else if input.just_pressed(KeyCode::KeyS) {
+        GridCoords::new(0, -1)
+    } else if input.just_pressed(KeyCode::KeyD) {
+        GridCoords::new(1, 0)
+    } else {
+        return;
+    };
+
+    for mut player_grid_coords in players.iter_mut() {
+        let destination = *player_grid_coords + movement_direction;
+        *player_grid_coords = destination;
+    }
+}
+
+fn translate_grid_coords_entities(
+    mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
+) {
+    for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
+        transform.translation = grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
+            .extend(transform.translation.z)
     }
 }
